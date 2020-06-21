9266543aeeb60b9c39e8262f037cee48a661c2eb
yaskalnazar <yaskalnazar@gmail.com>
2020-06-21 20:38:29 +0300

    two consumers

---
+++ src/main/java/lab4/rabbit/queue/consumer/WithoutAckConsolePrinterConsumer.java
@@ -0,0 +1,36 @@
+package lab4.rabbit.queue.consumer;
+
+import com.rabbitmq.client.Delivery;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import static lab4.rabbit.Utils.sleep;
+
+public class WithoutAckConsolePrinterConsumer extends AbstractConsumer{
+
+    public WithoutAckConsolePrinterConsumer(String host,
+                                            Integer hostPort,
+                                            String queueName,
+                                            Boolean durable,
+                                            Map<String, Object> queueArgs,
+                                            Long latency) {
+        super(host, hostPort, queueName, false, durable, queueArgs, latency);
+    }
+
+    @Override
+    protected void handleMessage(String consumerTag, Delivery rawMessage) throws IOException {
+        channel.basicQos(1);
+        String message = new String(rawMessage.getBody(), StandardCharsets.UTF_8);
+        System.out.println("Received '" + message + "'");
+        try {
+            sleep(latency);
+        }
+        finally {
+            System.out.println("Done");
+            //channel.basicAck(rawMessage.getEnvelope().getDeliveryTag(), false);
+        }
+    }
+
+}
--- src/main/java/lab4/rabbit/queue/runners/ack/WithoutAckConsumerRunner.java
+++ src/main/java/lab4/rabbit/queue/runners/ack/WithoutAckConsumerRunner.java
@@ -1,6 +1,7 @@
 package lab4.rabbit.queue.runners.ack;
 
-import lab4.rabbit.queue.producer.Producer;
+import lab4.rabbit.queue.consumer.AbstractConsumer;
+import lab4.rabbit.queue.consumer.WithoutAckConsolePrinterConsumer;
 
 import java.io.IOException;
 
@@ -8,16 +9,16 @@ import static lab4.rabbit.NetworkConstants.HOST;
 import static lab4.rabbit.NetworkConstants.PORT;
 import static lab4.rabbit.queue.Constants.ACK_QUEUE;
 
-public class AckProducerRunner {
+public class WithoutAckConsumerRunner {
 
     public static void main(String[] args) throws IOException {
-        Producer producer = new Producer(HOST,
+        AbstractConsumer consumer = new WithoutAckConsolePrinterConsumer(HOST,
                 PORT,
                 ACK_QUEUE,
                 false,
-                null);
+                null,
+                5000L);
 
-        producer.produce(11, null, 2500L);
+        consumer.start();
     }
-
 }
